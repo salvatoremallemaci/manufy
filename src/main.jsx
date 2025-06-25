0f836d8f183fa736717df519ef9290521782d44b
Salvatore Mallemaci <salvatore.mallemaci@pro-logic.it>
2025-06-25 21:43:32 +0200

    feat: updated GitHub Pages files

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import PlayerContextProvider from "./context/PlayerContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/manufy/">
       <PlayerContextProvider>
         <App />
       </PlayerContextProvider>
