466150e8197439718f44e9ce4bcdbae7a643d426
Salvatore Mallemaci <salvatore.mallemaci@pro-logic.it>
2025-07-02 22:02:48 +0200

    feat: responsive layout on songs

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,11 +1,17 @@
 import React, { useContext } from 'react'
 import Navbar from './Navbar'
 import { useParams } from 'react-router-dom'
-import { albumsData, assets, songsData, songsManuData, songsFedeData, songsSimoData, songsCarlottaData } from '../assets/assets';
+import {
+    albumsData,
+    assets,
+    songsManuData,
+    songsFedeData,
+    songsSimoData,
+    songsCarlottaData
+} from '../assets/assets';
 import { PlayerContext } from '../context/PlayerContext';
 
 const DisplayAlbum = () => {
-
     const { id } = useParams();
     const albumDatalocal = albumsData[id];
     const { playWithId } = useContext(PlayerContext);
@@ -48,29 +54,66 @@ const DisplayAlbum = () => {
                     </p>
                 </div>
             </div>
-            <div className='grid grid-cols-[3fr_1.5fr_1.5fr_1fr] mt-10 mb-4 pl-2 text-[#a7a7a7]'>
-                <p><b className='mr-4'>#</b>Title</p>
+
+            {/* HEADER */}
+            <div className='hidden md:grid grid-cols-[3fr_1.5fr_1.5fr_1fr] mt-10 mb-4 pl-2 text-[#a7a7a7]'>
+                <p className='ml-4'><b className='mr-4'>#</b>Title</p>
                 <p>Playlist</p>
-                <p className='hidden md:block'>Date Added</p>
-                <img className='m-auto w-4' src={assets.clock_icon} alt="" />
+                <p>Date Added</p>
+                <img className='m-auto w-4 mr-4' src={assets.clock_icon} alt="Duration" />
             </div>
+
+            {/* MOBILE HEADER */}
+            <div className='flex justify-between items-center md:hidden mt-10 mb-4 pl-2 pr-4 text-[#a7a7a7]'>
+                <p><b className='mr-4'>#</b>Title</p>
+                <img className='w-4' src={assets.clock_icon} alt="Duration" />
+            </div>
+
             <hr />
+
+            {/* SONGS */}
             {
                 songsData.map((item, index) => (
-                    <div onClick={() => playWithId(item.id, songsData)} key={index} className='grid grid-cols-[3fr_1.5fr_1.5fr_1fr] gap-3 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer'>
-                        <p className='tetx-white'>
-                            <span className='inline-block w-6 text-right mr-4 text-[#a7a7a7]'>
+                    <div
+                        onClick={() => playWithId(item.id, songsData)}
+                        key={index}
+                        className='md:grid md:grid-cols-[3fr_1.5fr_1.5fr_1fr] gap-3 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer flex justify-between'
+                    >
+                        {/* IMMAGINE + TITOLO + NUMERO */}
+                        <div className='flex items-center'>
+                            {/* Numero su mobile */}
+                            <span className='inline-block md:hidden text-[13px] w-4 mr-2 text-[#a7a7a7]'>
                                 {index + 1}
                             </span>
-                            <img className='inline w-10 mr-5' src={item.image} alt="" />
-                            {item.name}
-                        </p>
-                        <p className='text-[15px] ml-[0.3rem]'>{albumDatalocal.name}</p>
-                        <p className='text-[15px] ml-[0.5rem]'>3 days ago</p>
-                        <p className='text-[15px] ml-[4rem]'>{item.duration}</p>
+
+                            {/* Numero su desktop */}
+                            <span className='hidden md:inline-block w-6 text-right mr-4 text-[#a7a7a7]'>
+                                {index + 1}
+                            </span>
+
+                            {/* Immagine */}
+                            <img className='w-10 h-10 mr-4 rounded' src={item.image} alt="" />
+
+                            {/* Titolo */}
+                            <div className='flex flex-col'>
+                                <p className='text-white text-sm'>{item.name}</p>
+                                {/* Album su mobile */}
+                                <p className='text-xs text-gray-400 md:hidden'>{albumDatalocal.name}</p>
+                            </div>
+                        </div>
+
+                        {/* Playlist solo desktop */}
+                        <p className='hidden md:block text-[15px]'>{albumDatalocal.name}</p>
+
+                        {/* Date added solo desktop */}
+                        <p className='hidden md:block text-[15px] ml-2'>3 days ago</p>
+
+                        {/* Durata */}
+                        <p className='text-[15px] ml-auto md:ml-0 justify-self-end'>{item.duration}</p>
                     </div>
                 ))
             }
+
         </>
     )
 }
