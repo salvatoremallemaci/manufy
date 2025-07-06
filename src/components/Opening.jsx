93d5dd9d376552113b7a518fe150916eda12a730
Salvatore Mallemaci <salvatore.mallemaci@pro-logic.it>
2025-07-06 19:58:49 +0200

    feat: added new intro

--- src/components/Opening.jsx
+++ src/components/Opening.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import background from '../assets/manu-intro.mp4'
 
 const Opening = () => {
+  const videoRef = useRef(null)
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.play().catch(() => { })
+    }
+  }, [])
+
   return (
     <div className='h-screen bg-[#121212] flex items-center justify-center'>
-        <video className='w-[75%] object-cover mix-blend-screen' src={background} preload='auto' autoPlay loop muted playsInline></video>
+      <video
+        ref={videoRef}
+        className='w-[90vw] md:w-[40%] object-cover mix-blend-screen'
+        src={background}
+        preload='auto'
+        autoPlay
+        loop
+        muted
+        playsInline
+      ></video>
     </div>
   )
 }
